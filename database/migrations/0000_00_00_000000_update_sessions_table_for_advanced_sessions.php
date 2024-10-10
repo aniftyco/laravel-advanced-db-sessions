1612b81d77561e471d892af8a13b0a1e28a920be
Josh Manders <josh@joshmanders.com>
2024-10-09 19:22:52 -0500

    fix migration

--- database/migrations/0000_00_00_000000_update_sessions_table_for_advanced_sessions.php
+++ database/migrations/0000_00_00_000000_update_sessions_table_for_advanced_sessions.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::table('sessions', function (Blueprint $table) {
             $table->dropColumn('last_activity');
+            $table->dropColumn('user_id');
 
             $table->json('location')->nullable();
+
+            $table->after('id', function (Blueprint $table) {
+                $table->foreignIdFor(\App\Models\User::class)->nullable()->index();
+            });
 
             $table->after('payload', function (Blueprint $table) {
                 $table->timestamp('last_active_at')->index();
@@ -29,10 +34,12 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('sessions', function (Blueprint $table) {
+            $table->dropColumn('user_id');
             $table->dropColumn('last_active_at');
             $table->dropColumn('location');
             $table->dropColumn('created_at');
 
+            $table->foreignId('user_id')->nullable()->index();
             $table->integer('last_activity')->index();
         });
     }
